1d1b1932c18f6609fc16c9b8500ca7bbe371f364
trai <ttran96@gatech.edu>
2019-05-09 18:51:13 -0400

    Add intro

--- src/app/bio/intro/intro.component.ts
+++ src/app/bio/intro/intro.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-intro',
@@ -7,7 +7,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntroComponent implements OnInit {
 
-  constructor() { }
+  hackathons = [
+    {
+      name: 'GT Hackathon (2018 Winner)'
+    },
+    {
+      name: 'GSU Hackathon'
+    },
+    {
+      name: 'Atlanta Hackathon'
+    },
+    {
+      name: 'Emory Hackathon'
+    },
+  ];
+
+  aiProj = [
+    {
+      name: 'Predictive Analytics (Macy\'s Technology)',
+      route: ''
+    },
+    {
+      name: 'Buzzcourse',
+      route: ''
+    },
+    {
+      name: 'Game AI',
+      route: ''
+    },
+    {
+      name: 'Knowledge-based AI',
+      route: ''
+    },
+    {
+      name: 'Robotics',
+      route: ''
+    },
+  ];
+
+  systemsProj = [
+    {
+      name: 'Full-stack Compiler',
+      route: ''
+    },
+    {
+      name: 'Operating System',
+      route: ''
+    },
+    {
+      name: 'Networking Architecture',
+      route: ''
+    },
+    {
+      name: 'Database System',
+      route: ''
+    },
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
