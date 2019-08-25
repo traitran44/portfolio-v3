e3ef2cd50de06714b2c54f16ab534d9b536e0167
trait <ttran96@gatech.edu>
2019-08-25 02:53:45 -0400

    Added A* Pathfinding

---
+++ src/app/ai/game/component/a-star/a-star.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-a-star',
+  templateUrl: './a-star.component.html',
+  styleUrls: ['./a-star.component.scss']
+})
+export class AStarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/ai/game/game.module.ts
+++ src/app/ai/game/game.module.ts
@@ -6,12 +6,14 @@ import {GameComponent} from './game.component';
 import {ShareModule} from '../../share/share.module';
 import { PathNetComponent } from './component/path-net/path-net.component';
 import { NavMeshComponent } from './component/nav-mesh/nav-mesh.component';
+import { AStarComponent } from './component/a-star/a-star.component';
 
 @NgModule({
   declarations: [
     GameComponent,
     PathNetComponent,
-    NavMeshComponent
+    NavMeshComponent,
+    AStarComponent
   ],
   imports: [
     CommonModule,
